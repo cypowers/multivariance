74ca254dde840cd9fcbaf44bd4068148b54e5477
cypowers <devin17@knou.ac.kr>
2018-05-14 22:41:40 +0900

    current

--- scripts/Q3.1.r
+++ scripts/Q3.1.r
@@ -1,12 +1,18 @@
+install.packages("GPArotation")
 library(psych)
 library(GPArotation)
 data <- read.table("data/3.1 data.txt", header = TRUE)
 data2 <- data[2:9]
 data2
+# 달리기 소요시간 조정
+data2$run50m <-max(data2$run50m) - data2$run50m
+data2
 summary(data2)
+
 data2.factor = principal(data2, rotate="none")
 names(data2.factor)
-data2.factor
+data2.factor$values
+plot(data2.factor$values, type="b")
 screeplot(data2.factor$loadings, type="lines", pch=19, main="Scree Plot")
 
 
